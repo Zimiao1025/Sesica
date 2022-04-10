c813685b621cdbf36fa6cc08c6db3d89fc1fca3a
Leon_02 <Leon_02@aliyun.com>
2022-04-10 10:24:43 +0800

    Update the pie fig.

--- src/plot/plot_int.py
+++ src/plot/plot_int.py
@@ -1,3 +1,5 @@
+from itertools import cycle
+
 import joblib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -31,32 +33,46 @@ def bar_fig(model_path, feats_name, fig_path):
 def pie_fig(model_path, recipe, fig_path):
     gbm = joblib.load(model_path)
     scores = gbm.feature_importances_
-    weight = scores / sum(scores)
-    print(weight)
-    fig, ax = plt.subplots(figsize=(9, 7), subplot_kw=dict(aspect="equal"))
+    color_sets = cycle(['crimson', 'navy', 'teal', 'darkorange', 'purple', 'gray', 'green', 'dodgerblue', 'gold',
+                        'lightcoral', 'red'])
+    def make_autopct(values):
+        def my_autopct(pct):
+            total = sum(values)
+            val = int(round(pct*total/100.0))
+            # 同时显示数值和占比的饼图
+            return '{p:.1f}% ({v:d})'.format(p=pct,v=val)
+        return my_autopct
+    # weight = scores / sum(scores)
+    # print(weight)
+    _, ax = plt.subplots(subplot_kw=dict(aspect="equal"))
     # startangle 设置方向
-    wedges, texts = ax.pie(weight, wedgeprops=dict(width=0.5), startangle=-40)
-
+    # wedges, texts = ax.pie(scores, wedgeprops=dict(width=0.5), startangle=-40)
+    # x_0 = [1,0,0,0] 
+    # ax.pie(x_0, radius=0.9,colors = 'w')
+    ax.pie(scores,startangle = 90,radius=1.3,pctdistance = 0.9,colors=color_sets,autopct=make_autopct(scores),textprops = {'fontsize':13, 'color':'k'})
+    
+    ax.legend(recipe, bbox_to_anchor=(1.0,1.0), loc='center left')
+    
     # 每一类别说明框
     # boxstyle框的类型，fc填充颜色,ec边框颜色,lw边框宽度
-    bbox_props = dict(boxstyle="square,pad=0.3", fc='white', ec="black", lw=0.72)
-    # 设置框引出方式
-    kw = dict(arrowprops=dict(arrowstyle="-"),
-              bbox=bbox_props, zorder=0, va="center")
+    # bbox_props = dict(boxstyle="square,pad=0.3", fc='white', ec="black", lw=0.72)
+    # # 设置框引出方式
+    # kw = dict(arrowprops=dict(arrowstyle="-"),
+    #           bbox=bbox_props, zorder=0, va="center")
 
-    # 添加标签
-    for i, p in enumerate(wedges):
-        ang = (p.theta2 - p.theta1) / 2. + p.theta1
-        y = np.sin(np.deg2rad(ang))
-        x = np.cos(np.deg2rad(ang))
-        # 设置方向
-        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
-        kw["arrowprops"].update({"connectionstyle": connectionstyle})
-        # 设置标注
-        ax.annotate(recipe[i], xy=(x, y), xytext=(1.1 * np.sign(x), 1.2 * y),
-                    horizontalalignment=horizontalalignment, color='black', **kw)
-
-    plt.title('Feature importance for LTR', fontsize=18)
+    # # 添加标签
+    # for i, p in enumerate(wedges):
+    #     ang = (p.theta2 - p.theta1) / 2. + p.theta1
+    #     y = np.sin(np.deg2rad(ang))
+    #     x = np.cos(np.deg2rad(ang))
+    #     # 设置方向
+    #     horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+    #     connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+    #     kw["arrowprops"].update({"connectionstyle": connectionstyle})
+    #     # 设置标注
+    #     ax.annotate(recipe[i], xy=(x, y), xytext=(1.1 * np.sign(x), 1.2 * y),
+    #                 horizontalalignment=horizontalalignment, color='black', **kw)
+    ax.text(0.1, 1.6, 'Feature importance for LTR', fontsize=18, ha='center',va='top',wrap=True)
+    # plt.title('Feature importance for LTR', fontsize=18)
     plt.savefig(fig_path, bbox_inches='tight')
     plt.close(0)
